000c7a99c7a8e0221d8c98c73a72d2f7110e1d38
TRIF3X <hoessly.conner@gmail.com>
2019-06-25 13:45:04 -0500

    finish e2e test

--- README.md
+++ README.md
@@ -17,3 +17,7 @@
 - Check for false positives
 - Check that the given input will give the desired output
 - Check inputs are valid
+
+
+## Defenitions:
+- Headless Browser: A headless browser is a web browser without a graphical user interface. Headless browsers provide automated control of a web page in an environment similar to popular web browsers, but are executed via a command-line interface or using network communication.
--- util.test.js
+++ util.test.js
@@ -1,4 +1,5 @@
-const { generateText } = require('./util.js');
+const puppeteer = require('puppeteer')
+const { generateText, checkAndGenerate } = require('./util.js');
 
 test('should output name and age', () => {
     const text = generateText('conner', 24);
@@ -15,3 +16,33 @@ test('should output data-less text', () => {
 
     expect(text2).toBe('undefined (undefined years old)')
 })
+
+test('should generate a valid text output', () => {
+    const text = checkAndGenerate('conner', 24);
+
+    expect(text).toBe('conner (24 years old)')
+})
+
+test('should create element with text and correct class', async () => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        // slowMo: 80,
+        // args: ['--window-size=1920,1080']
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto(
+        'http://127.0.0.1:5500/js-testing-introduction/index.html'
+    )
+
+    await page.click('input#name')
+    await page.type('input#name', 'timmy')
+    await page.click('input#age')
+    await page.type('input#age', '32')
+    await page.click('#btnAddUser')
+
+    const finalText = await page.$eval('.user-item', el => el.textContent)
+
+    expect(finalText).toBe('timmy (32 years old)')
+}, 10000)
